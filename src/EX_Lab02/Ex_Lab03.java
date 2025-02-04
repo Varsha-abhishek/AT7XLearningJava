df8890dc07d2848e5ee4f6731459eb285eeb3d43
Amit1251 <amitsinha3025@gmail.com>
2025-02-04 15:31:33 +0530

    All files

--- src/EX_Lab02/Ex_Lab03.java
+++ src/EX_Lab02/Ex_Lab03.java
@@ -1,7 +1,7 @@
 package EX_Lab02;
 
-public class cla002 {
+public class Ex_Lab03 {
     public static void main(String[] args) {
-        System.out.println("Varshaa");
+        System.out.println(" my!!");
     }
 }
